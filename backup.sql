79ad3f432c0f929e168da13a2dd153bac5608384
Muhammad Ahsan <m.ahsanrasheed85@gmail.com>
2019-09-14 10:12:13 +0500

    generate script of database

--- backup.sql
+++ backup.sql
@@ -13,60 +13,61 @@
 
 
 -- Dumping database structure for quizdatabase
+DROP DATABASE IF EXISTS `quizdatabase`;
 CREATE DATABASE IF NOT EXISTS `quizdatabase` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `quizdatabase`;
 
 -- Dumping structure for table quizdatabase.answers
+DROP TABLE IF EXISTS `answers`;
 CREATE TABLE IF NOT EXISTS `answers` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
   `answer` varchar(255) NOT NULL DEFAULT '',
   `question_id` varchar(255) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
--- Dumping data for table quizdatabase.answers: ~20 rows (approximately)
+-- Dumping data for table quizdatabase.answers: ~4 rows (approximately)
+DELETE FROM `answers`;
 /*!40000 ALTER TABLE `answers` DISABLE KEYS */;
 INSERT INTO `answers` (`id`, `answer`, `question_id`) VALUES
-	(1, 'Java', '1'),
-	(2, 'PHP', '1'),
-	(3, 'C', '1'),
-	(4, 'C#', '1'),
-	(5, '1', '2'),
-	(6, '2', '2'),
-	(7, 'No Limit', '2'),
-	(8, 'Depends on Compiler', '2'),
-	(9, 'Operator', '3'),
-	(10, 'Function', '3'),
-	(11, 'Macro', '3'),
-	(12, ' None of these', '3'),
-	(13, '10', '4'),
-	(14, 'Compilation Error', '4'),
-	(15, '0', '4'),
-	(16, 'Undefined', '4'),
-	(17, '20', '5'),
-	(18, '0', '5'),
-	(19, 'Undefined reference to i', '5'),
-	(20, 'Linking Error', '5');
+	(1, 'Fugiat laboris volup', '1'),
+	(2, 'Ut aut aut et quis', '1'),
+	(3, 'Et eiusmod in sunt ', '1'),
+	(4, 'Ea magnam sit disti', '1');
 /*!40000 ALTER TABLE `answers` ENABLE KEYS */;
 
 -- Dumping structure for table quizdatabase.questions
+DROP TABLE IF EXISTS `questions`;
 CREATE TABLE IF NOT EXISTS `questions` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
   `questions` varchar(255) NOT NULL DEFAULT '',
   `type` varchar(50) NOT NULL DEFAULT '0',
-  `answer_id` int(255) NOT NULL,
+  `answer_id` int(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
--- Dumping data for table quizdatabase.questions: ~5 rows (approximately)
+-- Dumping data for table quizdatabase.questions: ~1 rows (approximately)
+DELETE FROM `questions`;
 /*!40000 ALTER TABLE `questions` DISABLE KEYS */;
 INSERT INTO `questions` (`id`, `questions`, `type`, `answer_id`) VALUES
-	(1, 'Which programming language is more faster among these?', 'c', 3),
-	(2, 'How many main() function we can have in our project?', 'c', 2),
-	(3, 'What is sizeof() in C?', 'c', 9),
-	(4, 'main()\r\n{\r\n     int x = 10;\r\n	{\r\n	    int x = 0;\r\n	    printf("%d",x);\r\n	}\r\n}', 'c', 15),
-	(5, 'int main()\r\n{\r\nextern int i;\r\ni = 20;\r\nprintf("%d", sizeof(i));\r\nreturn 0;\r\n}\r\n', 'c', 19);
+	(1, 'Omnis deserunt itaqu', 'c', NULL);
 /*!40000 ALTER TABLE `questions` ENABLE KEYS */;
+
+-- Dumping structure for table quizdatabase.users
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- Dumping data for table quizdatabase.users: ~0 rows (approximately)
+DELETE FROM `users`;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+	(1, 'Ahsan85', 'admin');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
